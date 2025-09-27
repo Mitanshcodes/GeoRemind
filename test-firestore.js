ba5b1f251490719e5d4f426c8674e69e57e3865b
Aadi Juthani <aadi.juthani@gatech.edu>
2025-09-27 18:58:46 -0400

    Delete test-firestore.js

--- test-firestore.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-admin.initializeApp();
-const db = admin.firestore();
-(async () => {
-  const ref = db.collection('test').doc('smoke');
-  await ref.set({ ok: true, ts: new Date().toISOString() });
-  const snap = await ref.get();
-  console.log('wrote:', snap.data());
-})();
